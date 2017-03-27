cbcf0c91d064c34bbc1a97e8324d14c6ffd14705
CHOM\chommchome <chompunarakjung18@gmail.com>
2017-03-27 19:28:43 +0700

    Add payment layout

--- app/src/main/java/com/example/kaew_pc/diary_project/PaymentManagement/PaymentMainPage.java
+++ app/src/main/java/com/example/kaew_pc/diary_project/PaymentManagement/PaymentMainPage.java
@@ -1,11 +1,15 @@
 package com.example.kaew_pc.diary_project.PaymentManagement;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.example.kaew_pc.diary_project.R;
 
@@ -18,9 +22,22 @@ public class PaymentMainPage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.payment_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+//                Intent intent;
+//                intent = new Intent(getApplicationContext(), PaymentActivity.class);
+//                startActivity(intent);
+
+            }
+        });
 
     }
 }
